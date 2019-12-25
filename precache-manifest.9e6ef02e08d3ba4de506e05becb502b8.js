2899321299d65e827b019e3ab9232b5e2b016cd1
Oles Marola <oles_pw@ukr.net>
2019-12-25 17:18:23 +0200

    Updates

---
+++ precache-manifest.9e6ef02e08d3ba4de506e05becb502b8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6219048c1f45169fcae59691c67d2d01",
+    "url": "/react_tabs-with-router/index.html"
+  },
+  {
+    "revision": "3eb6d0b8af5013e18824",
+    "url": "/react_tabs-with-router/static/js/2.cc1e8166.chunk.js"
+  },
+  {
+    "revision": "4c71b14b2f8444a27a64",
+    "url": "/react_tabs-with-router/static/js/main.c0130710.chunk.js"
+  },
+  {
+    "revision": "da3ca6fceb301548d22d",
+    "url": "/react_tabs-with-router/static/js/runtime~main.5858125e.js"
+  }
+]);
